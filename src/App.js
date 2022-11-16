9add040d854cef1958c69307aec37644f8c98c0f
Rhys Faultless <rfaultless@clearpath.ai>
2022-11-16 15:01:40 -0500

    Added React Router and multiple pages. All pages will load, regardless of User's group / permissions.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+  
+// import Home component
+import HomePage from "./HomePage";
+import PageA from "./PageA";
+import PageB from "./PageB";
 
 import { Amplify, Auth } from 'aws-amplify';
 // import awsconfig from './aws-exports';
@@ -38,9 +49,34 @@ export default App;
 function App({ signOut, user }) {
   return (
     <>
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
+      <Router>
+        <div>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+
+          <hr />
+          
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/pagea">Page A</Link>
+            </li>
+            <li>
+              <Link to="/pageb">Page B</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/pagea" element={<PageA />} />
+            <Route path="/pageb" element={<PageB />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from './logo.svg';
+  
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+};
+  
+export default HomePage;
