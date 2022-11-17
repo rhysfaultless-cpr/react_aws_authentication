849ee13572892d38a5cceb4846abe47800d62a24
Rhys Faultless <rfaultless@clearpath.ai>
2022-11-17 10:27:23 -0500

    Added Local State for User's AWS information. Local State allows React to update after Async call.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
 import './App.css';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
-  
-// import Home component
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import { useState, useEffect } from 'react';
+
 import HomePage from "./HomePage";
 import PageA from "./PageA";
 import PageB from "./PageB";
 
 import { Amplify, Auth } from 'aws-amplify';
-// import awsconfig from './aws-exports';
-// Amplify.configure(awsconfig);
-
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-//const currentUser = Auth.currentAuthenticatedUser();
-const curerentUser = Auth.currentAuthenticatedUser({
-  bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-}).then(
-  user => console.log(user)
-)
-.catch(err => console.log(err));
-//console.log(currentUser['signInUserSession']['accessToken']['payload']['cognito:groups'])
+async function fetchTenant(setTenant) {
+  // get the access token of the signed in user
+  const {accessToken} = await Auth.currentSession();
+  // get the tenant from the top of the cognito groups list
+  const cognitogroups = accessToken.payload['cognito:groups'];
+  const tenant = cognitogroups[0];
+  setTenant(tenant);
+}
+
 
 function App({ signOut, user }) {
+
+  const [tenant, setTenant] = useState('');
+  useEffect(() => {
+    fetchTenant(setTenant);
+  }, []);
 
   return (
     <>
       <Router>
         <div>
           <h1>Hello {user.username}</h1>
-
+          <p>Current User's Groups: {tenant}</p>
           <button onClick={signOut}>Sign out</button>
 
           <hr />
