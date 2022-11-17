c56fb8d03069991fcaa594dc9588728e80321b26
Rhys Faultless <rfaultless@clearpath.ai>
2022-11-17 11:06:38 -0500

    Added functions and State to change routing, based on User's Groups

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { useState, useEffect } from 'react';
 import HomePage from "./HomePage";
 import PageA from "./PageA";
 import PageB from "./PageB";
+import PageBlocked from "./PageBlocked";
 
 import { Amplify, Auth } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
@@ -14,21 +15,27 @@ import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-async function fetchTenant(setTenant) {
+async function fetchTenant(setTenant, setUserIsCustomer, setUserIsReseller, setUserIsEmployee) {
   // get the access token of the signed in user
   const {accessToken} = await Auth.currentSession();
   // get the tenant from the top of the cognito groups list
   const cognitogroups = accessToken.payload['cognito:groups'];
   const tenant = cognitogroups[0];
   setTenant(tenant);
+  setUserIsCustomer(tenant.indexOf('customer') !== -1);
+  setUserIsReseller(tenant.indexOf('reseller') !== -1);
+  setUserIsEmployee(tenant.indexOf('employee') !== -1);
 }
-
 
 function App({ signOut, user }) {
 
   const [tenant, setTenant] = useState('');
+  const [userIsCustomer, setUserIsCustomer] = useState(false);
+  const [userIsReseller, setUserIsReseller] = useState(false);
+  const [userIsEmployee, setUserIsEmployee] = useState(false);
+
   useEffect(() => {
-    fetchTenant(setTenant);
+    fetchTenant(setTenant, setUserIsCustomer, setUserIsReseller, setUserIsEmployee);
   }, []);
 
   return (
@@ -36,7 +43,9 @@ function App({ signOut, user }) {
       <Router>
         <div>
           <h1>Hello {user.username}</h1>
-          <p>Current User's Groups: {tenant}</p>
+          <p>Current User's Group includes 'customer': {String(tenant.indexOf('customer') !== -1)}</p>
+          <p>Current User's Group includes 'reseller': {String(tenant.indexOf('reseller') !== -1)}</p>
+          <p>Current User's Group includes 'employee': {String(tenant.indexOf('employee') !== -1)}</p>
           <button onClick={signOut}>Sign out</button>
 
           <hr />
@@ -57,8 +66,10 @@ function App({ signOut, user }) {
 
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/pagea" element={<PageA />} />
-            <Route path="/pageb" element={<PageB />} />
+            {!userIsCustomer && <Route path="/pagea" element={<PageBlocked />} />}
+            {userIsCustomer && <Route path="/pagea" element={<PageA />} />}
+            {!userIsEmployee && <Route path="/pageb" element={<PageBlocked />} />}
+            {userIsEmployee && <Route path="/pageb" element={<PageB />} />}
           </Routes>
         </div>
       </Router>
